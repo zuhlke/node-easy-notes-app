8bff07749772b717e2dff0c4c53a37f563f7f382
Immo Hüneke <immo.huneke@zuhlke.com>
2018-08-09 13:23:13 +0100

    Rename scripts appropriately

--- scripts/app_setup.sh
+++ scripts/app_setup.sh
@@ -2,4 +2,9 @@
 
 cd "${0%/*}"
 . common_config.sh
-kubectl delete service,deployment easy-notes-app
+docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 .
+gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1
+kubectl run easy-notes --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 --port=8080
+kubectl get deployments
+kubectl get pods
+kubectl cluster-info
--- scripts/app_teardown.sh
+++ scripts/app_teardown.sh
@@ -2,9 +2,4 @@
 
 cd "${0%/*}"
 . common_config.sh
-docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 .
-gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1
-kubectl run easy-notes --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 --port=8080
-kubectl get deployments
-kubectl get pods
-kubectl cluster-info
+kubectl delete service,deployment easy-notes-app
