6c506880b85e8c16489ae6c0dd0842f2ad6b845d
Immo Hüneke <immo.huneke@zuhlke.com>
2018-08-09 13:26:37 +0100

    Rename deployment; add service

--- scripts/app_setup.sh
+++ scripts/app_setup.sh
@@ -4,7 +4,8 @@ cd "${0%/*}"
 . common_config.sh
 docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 .
 gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1
-kubectl run easy-notes --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 --port=8080
+kubectl run easy-notes-app --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-app:v1 --port=8080
 kubectl get deployments
 kubectl get pods
 kubectl cluster-info
+kubectl expose deployment easy-notes-app --type="LoadBalancer"
